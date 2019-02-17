47f393d404b441e19b8058e3e3e6ede1c34ad9ae
Waleed Chaudhry <waleed.u.chaudhry@gmail.com>
2019-02-17 02:26:58 -0500

    Create mnemonicToChildPrivateKey.js

---
+++ mnemonicToChildPrivateKey.js
@@ -0,0 +1,12 @@
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+
+const mnemonic = 'split logic consider degree smile field term style opera dad believe indoor item type beyond'
+const seed = bip39.mnemonicToSeed(mnemonic)
+console.log(seed.toString('hex'))
+// f8355cdca603c05231502f26e8f36134fd6723a90aa544160322be69236912f93e14d015ca448b1fcafe2685db2a571c3006c3b280d6885a3fd3c9ec3e9b62d7
+const path = 'm/0/0'
+const node = bip32.fromSeed(seed)
+const child1 = node.derivePath(path)
+console.log(child1.toWIF())
+// L1T5525SbSDjM1DzMfLXx9CSjHVCicVH5cectxEbrwxQdvqxEwL8
